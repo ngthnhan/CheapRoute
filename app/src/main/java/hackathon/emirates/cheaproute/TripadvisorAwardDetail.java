03128b5eccd0b5187a3eb7076d51259b2fb6b062
ngthnhan <ngthnhan93@gmail.com>
2015-03-14 11:54:51 +0800

    Added basic Tripadvisor_ classes

---
+++ app/src/main/java/hackathon/emirates/cheaproute/TripadvisorAwardDetail.java
@@ -0,0 +1,44 @@
+package hackathon.emirates.cheaproute;
+
+/**
+ * Created by nhan on 3/14/15.
+ *         {
+ "award_type": "Certificate of Excellence",
+ "year": "2014",
+ "images": {
+ "small": "http://www.tripadvisor.com/img/cdsi/img2/awards/CERTIFICATE_OF_EXCELLENCE_small-MCID-5.jpg",
+ "large": "http://www.tripadvisor.com/img/cdsi/img2/awards/CERTIFICATE_OF_EXCELLENCE_2014_en_US_large-MCID-5.jpg"
+ },
+ "categories": [],
+ "display_name": "Certificate of Excellence 2014"
+ }
+ */
+public class TripadvisorAwardDetail {
+    private String award_type;
+    private String year;
+    private String display_name;
+
+    public String getAward_type() {
+        return award_type;
+    }
+
+    public void setAward_type(String award_type) {
+        this.award_type = award_type;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getDisplay_name() {
+        return display_name;
+    }
+
+    public void setDisplay_name(String display_name) {
+        this.display_name = display_name;
+    }
+}
---
+++ app/src/main/java/hackathon/emirates/cheaproute/TripadvisorImages.java
@@ -0,0 +1,29 @@
+package hackathon.emirates.cheaproute;
+
+/**
+ * Created by nhan on 3/14/15.
+ *  "images": {
+ "small": "http://www.tripadvisor.com/img/cdsi/img2/awards/CERTIFICATE_OF_EXCELLENCE_small-MCID-5.jpg",
+ "large": "http://www.tripadvisor.com/img/cdsi/img2/awards/CERTIFICATE_OF_EXCELLENCE_2014_en_US_large-MCID-5.jpg"
+ },
+ */
+public class TripadvisorImages {
+    private String small;
+    private String large;
+
+    public String getSmall() {
+        return small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+    public String getLarge() {
+        return large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
+}
