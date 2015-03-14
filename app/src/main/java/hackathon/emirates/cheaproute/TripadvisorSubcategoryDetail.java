c5ce021c39a06813937e772f8a54ba3d79677c3f
ngthnhan <ngthnhan93@gmail.com>
2015-03-14 12:21:00 +0800

    Added TripadvisorSubcategoryDetail

---
+++ app/src/main/java/hackathon/emirates/cheaproute/TripadvisorSubcategoryDetail.java
@@ -0,0 +1,11 @@
+package hackathon.emirates.cheaproute;
+
+/**
+ * Created by nhan on 3/14/15.
+ */
+public class TripadvisorSubcategoryDetail extends TripadvisorCategoryDetail{
+
+    public TripadvisorSubcategoryDetail(String name, String localized_string) {
+        super(name, localized_string);
+    }
+}
