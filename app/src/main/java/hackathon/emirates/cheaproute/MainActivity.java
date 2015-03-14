f873e3bb9dd5edc0926d3f6549cac0ac86d58c0b
Tran Manh Tu <xxlaguna93@gmail.com>
2015-03-14 12:31:10 +0800

    asynctask

--- app/src/main/java/hackathon/emirates/cheaproute/MainActivity.java
+++ app/src/main/java/hackathon/emirates/cheaproute/MainActivity.java
@@ -36,4 +36,47 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private class CallAPI_1 extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            String resultToDisplay = "";
+            List<NameValuePair> queryParams = new ArrayList<>();
+            queryParams.add(new BasicNameValuePair("key", API_KEY));
+            queryParams.add(new BasicNameValuePair("address", params[0]));
+
+            String urlString = ROOT_URL + URLEncodedUtils.format(queryParams, "UTF-8");
+
+            try {
+                URL url = new URL(urlString);
+
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                LocationResponse response = mapper.readValue(url, LocationResponse.class);
+
+                switch (response.getStatus()) {
+                    case "OK":
+                        LocationData location = response.getResults()[0]; // Take first one
+                        // Only care about the first result
+                        resultToDisplay = location.displayResult();
+                        break;
+                    default:
+                        System.out.println("Status is not okay" + response.getStatus());
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return resultToDisplay;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            TextView resultView = (TextView) findViewById(R.id.resultStr);
+            resultView.setText(result);
+        }
+    }
 }
